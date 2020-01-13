54e243a6950fc9652dbbb48405e27d7b1a12dd7a
lapiscine <atif.zourgani@lapiscine.com>
2020-01-13 16:45:28 +0100

    sauvegarde

--- src/Controller/galerie/galerieController.php
+++ src/Controller/galerie/galerieController.php
@@ -9,6 +9,7 @@ use App\Repository\GalerieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class galerieController extends AbstractController
@@ -22,7 +23,7 @@ class galerieController extends AbstractController
     {
         $date = $request->query->get('date');
 
-        $galerie = $galerieRepository->getGalerieOrderByDate($date);
+        $galerie = $galerieRepository->findBy([],['date' => 'DESC']);
 
         return $this->render('galerie/galerie.html.twig', ['galerie' => $galerie]);
     }
@@ -45,7 +46,7 @@ class galerieController extends AbstractController
         $description = $request->query->get('description');
         $name = $request->query->get('name');
 
-        $galerie = $galerieRepository->getGalerieByDescriptionOrByName ($description, $name);
+        $galerie = $galerieRepository->getGalerieByDescriptionOrByName($description, $name);
 
         return $this->render('galerie/galerie.html.twig', ['galerie' => $galerie]);
     }
@@ -69,16 +70,46 @@ class galerieController extends AbstractController
 
     /**
      * @Route("/accueil/galerie/galerie_insert", name="galerie_insert")
+     * @param Request $request
+     * @return Response
      */
-    public function insertGalerie()
+    public function insertGalerie(Request $request)
     {
-        return $this->render('galerie/galerie_insert.html.twig');
+        $galerie = new Galerie();
+        $form = $this->createForm(GalerieType::class, $galerie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // stock toutes les images envoyées par le formulaire dans $images
+            $image = $request->files->get('galerie')['image'];
+            if ($image != null) {
+                // stock le dossier de destination défini dans config/services.yaml
+                $upload_directory = $this->getParameter('upload_directory');
+                $filename = $request->request->get('galerie')['img'] . '.' . $image->guessExtension();
+
+                // déplace le fichier dans le dossier désiré
+                $image->move(
+                // dossier de destination
+                    $upload_directory,
+                    // nom du fichier
+                    $filename);
+                // Ajoute le nom du fichier image à l'array Images dans la BDD
+            }
+            $galerie->setImg($filename);
+            $galerie->setDate(new \DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($galerie);
+            $entityManager->flush();
+            return $this->redirectToRoute('galerie');
+        }
+        return $this->render('galerie/galerie_insert.html.twig', ['galerie' => $galerie,
+            'form' => $form->createView(),]);
     }
 
     /**
      * @Route("/accueil/galerie/galerie_insert_ok", name="galerie_insert_ok")
      */
-    public function insertGalerieOk(EntityManagerInterface $entityManager, Request $request)
+    public
+    function insertGalerieOk(EntityManagerInterface $entityManager, Request $request)
     {
         $name = $request->query->get('name');
         $description = $request->query->get('description');
@@ -91,7 +122,7 @@ class galerieController extends AbstractController
         $galerie->setName($name);
         $galerie->setDescription($description);
         $galerie->setAlt($alt);
-        $galerie->setDate( new \DateTime($date));
+        $galerie->setDate(new \DateTime($date));
         $galerie->setAuthor($authors);
 
         $entityManager->persist($galerie);
@@ -104,7 +135,8 @@ class galerieController extends AbstractController
     /**
      * @Route("/accueil/galerie/update/{id}", name="galerie_update_id")
      */
-    public function updateGalerie (GalerieRepository $galerieRepository, $id)
+    public
+    function updateGalerie(GalerieRepository $galerieRepository, $id)
     {
         $galerie = $galerieRepository->find($id);
 
@@ -114,7 +146,8 @@ class galerieController extends AbstractController
     /**
      * @Route("/accueil/galerie/update", name="galerie_update_ok")
      */
-    public function updateGalerieOk (EntityManagerInterface $entityManager, Request $request, GalerieRepository $galerieRepository, $id)
+    public
+    function updateGalerieOk(EntityManagerInterface $entityManager, Request $request, GalerieRepository $galerieRepository, $id)
     {
         $galerie = $galerieRepository->find($id);
 
@@ -129,7 +162,7 @@ class galerieController extends AbstractController
         $galerie->setName($name);
         $galerie->setDescription($description);
         $galerie->setAlt($alt);
-        $galerie->setDate( new \DateTime($date));
+        $galerie->setDate(new \DateTime($date));
         $galerie->setAuthor($authors);
 
         $entityManager->persist($galerie);
