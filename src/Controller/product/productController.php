5d0e306e52c99fdeca1ec4e83a1d9c3c64c9df5e
lapiscine <atif.zourgani@lapiscine.com>
2020-01-15 10:50:06 +0100

    sauvegarde

--- src/Controller/product/productController.php
+++ src/Controller/product/productController.php
@@ -3,17 +3,18 @@
 
 namespace App\Controller\product;
 
-use App\Entity\Product;
 use App\Form\ProductType;
-use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class productController extends AbstractController
 
 {
-
+    /**
+     * @Route("accueil/product", name="product")
+     */
+    public function allProduct()
+    {
+        return $this->render('product/product.html.twig');
+    }
 }
